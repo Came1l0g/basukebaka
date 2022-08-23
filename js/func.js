9efeb79d95d8170bc0d546405d05fa487c7f6f44
came1l0g <came1l0g.ubuntu@gmail.com>
2022-08-23 22:30:27 +0900

    整形

--- js/func.js
+++ js/func.js
@@ -3,17 +3,17 @@ let prefe = document.getElementById('prefe');
 let munici = document.getElementById('munici');
 
 // 都道府県のセレクトボックスに値を入れる（関数化）
-function addition (ele1, ele2) {
+function addition(ele1, ele2) {
     ele1.forEach(element => {
         let options = document.createElement('option');
-        if(element['code']){
+        if (element['code']) {
             options.value = element['code'];
             options.textContent = element['name'];
         } else {
             options.value = element['id'];
             options.textContent = element['name'];
         }
-        ele2.appendChild(options);  
+        ele2.appendChild(options);
     });
 
 }
@@ -22,20 +22,20 @@ function addition (ele1, ele2) {
 addition(prefecture, prefe);
 
 // セレクトボックスの値が変わったら市口調損のデータを取得させる
-prefe.addEventListener('change', function() {
+prefe.addEventListener('change', function () {
     console.log(prefe.value);
     getApi(prefe.value);
 });
 
 // 市区町村のデータを取得
-function getApi (number) {
+function getApi(number) {
     const api = `https://www.land.mlit.go.jp/webland/api/CitySearch?area=${number}`;
     fetch(api)
         .then((res) => {
-            return(res.json());
+            return (res.json());
         })
         .then((json) => {
-            while(munici.firstChild){
+            while (munici.firstChild) {
                 munici.removeChild(munici.firstChild);
             }
             addition(json['data'], munici);
